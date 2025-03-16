08b746ea986f8a746cd1a78f27f1a6c84a484f55
Manifoldz <56.zabolotnyi@gmail.com>
2025-03-16 20:44:38 +0500

    started main

--- main.cc
+++ main.cc
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <string>
 #include <unordered_map>
 #include <vector>
@@ -20,29 +21,26 @@ typedef struct Dialog {
   std::vector<Message> messages;
 };
 
-enum Key { DISPLAY_NAME, FROM, TIME, CONTENT };
+enum TypeKey { DIALOG_DNAME, MSG_DNAME, FROM, TIME, CONTENT };
 
 int main(void) {
-  std::unordered_map<std::string, Key> allowed_keys = {
-      {"from", FROM}, {"originalarrivaltime", TIME}, {"content", CONTENT}};
-
   // Хеш-таблица уникальных названий ключей в json, которые нас интересуют
+  std::unordered_map<std::string, TypeKey> allowed_keys = {
+      {"displayName", DIALOG_DNAME},
+      {"from", FROM},
+      {"originalarrivaltime", TIME},
+      {"content", CONTENT}};
 
+  // Хеш-таблица уникальных авторов, ключ это "from", а значение "displayName"
   std::unordered_map<std::string, std::string> authors;
-  // Хеш-таблица уникальных авторов ключ это "from", а значение это
-  // "displayName"(может быть пустым)
 
-  std::vector<Dialog> dialogs;  //   вектор с диалогами
+  // Все диалоги
+  std::vector<Dialog> dialogs;
 
-  // Выделяем буффер чаров у себя под ключи сообщений отсюда проверять будем
-  // с строками в allowed_keys. Также этот же буффер используется под значения
+  // Выделяем буффер под ключи сообщений - отсюда будем сверять их
+  // с строками в allowed_keys. Также этот же буффер используется для значений
   // TODO: надо выяснить макс длину в скайпе
   char buffer[MAX_BUF_SIZE + 1] = {'\0'};
-
-  // Выделяем буффер под отображаемое имя так как в случае сообщения сначала
-  // надо будет добраться до "from" проверить его наличие уже в хеш-таблице и
-  // если его нет добавить его и отображаемое имя в хеш-таблицу
-  char display_name_buffer[MAX_DNAME_SIZE + 1] = {'\0'};
 
   char symbol = '\0';
   int len_buffer = 0;
@@ -55,53 +53,59 @@ int main(void) {
     if (symbol == '"') {
       // перекладываем в буффер символы, пока там есть место и не встретим
       // закрывающие кавычки или конец файла
-      while (len_buffer < MAX_KEY_SIZE && (symbol = getchar()) != '"' &&
+      while (len_buffer < MAX_BUF_SIZE && (symbol = getchar()) != '"' &&
              symbol != EOF) {
         buffer[len_buffer++] = symbol;
       }
       buffer[len_buffer] = '\0';
+
       // проверяем ключ на наличие в списке ключей которые нас интересуют
-      // if (ключ есть в allowed_keys) {
-      //    сохраняем какой это ключ
-      //    Key key = allowed_keys[ключ];
-      //
-      //    char prev_symbol = '\0';
-      //    len_buffer =0;
-      //    if (getchar() != ':') {
-      //        выход?
-      //    }
-      //    char last_symbol = '"';
-      //    if ((symbol = getchar()) == 'n') {
-      //        last_symbol = ',';
-      //    }
-      //
-      //    пока не встретим символ last_symbol который не экранируется и не
-      //    конец while((symbol = getchar()) != EOF && !(symbol == last_symbol
-      //    && prev_symbol == '\')) {
-      //        buffer[len_buffer++] = symbol;
-      //        prev_symbol = symbol;
-      //    }
-      //
-      //    вот тут надо понять и как-то закинуть получившееся значение
-      //    в хеш-таблицу, просто там сейчас я указал хранится указатель
-      //    на строку а может все таки просто строку которую тут вот получили
-      //    копировать из буффера в хеш-таблицу под соответствующий ключ
-      //    ключ - то есть под какое поле класть в структуру message мы
-      //    определяем по значению enum из хеш таблицы
-      //
-      //    вообще лучше отдельную функцию по добавлению
-      //    save_token(buffer, dialogs, key);
-      // }
+      auto iter = allowed_keys.find(std::string(buffer));
+      if (iter != allowed_keys.end()) {
+        // сохраняем какой это ключ из enum
+        TypeKey typekey = iter->second;
+
+        // выходим если не двоеточие
+        if (getchar() != ':') {
+          std::cout << "Wrong char after key:" << buffer << "\n";
+          return 1;
+        }
+
+        char border_symbol = '"';  // до какого символа читаем значение
+        if ((symbol = getchar()) == 'n') {
+          border_symbol = ',';
+        }
+
+        char prev_symbol = '\0';
+        len_buffer = 0;  // переиспользуем буффер
+
+        // пока не встретим '"', который не экранируется или ',' или конец
+           while((symbol = getchar()) != EOF && !(symbol == border_symbol && prev_symbol == '\'))) {
+               buffer[len_buffer++] = symbol;
+               prev_symbol = symbol;
+      }
     }
-    len_buffer = 0;
+    //
+    //    вот тут надо понять и как-то закинуть получившееся значение
+    //    в хеш-таблицу, просто там сейчас я указал хранится указатель
+    //    на строку а может все таки просто строку которую тут вот получили
+    //    копировать из буффера в хеш-таблицу под соответствующий ключ
+    //    ключ - то есть под какое поле класть в структуру message мы
+    //    определяем по значению enum из хеш таблицы
+    //
+    //    вообще лучше отдельную функцию по добавлению
+    //    save_token(buffer, dialogs, key);
+    // }
   }
+  len_buffer = 0;
+}
 
-  //   for (по количеству диалогов в dialogs) {
-  //     // TODO: печать каждого диалога в отдельном файле
-  //     print_dialog();
-  //   }
+//   for (по количеству диалогов в dialogs) {
+//     // TODO: печать каждого диалога в отдельном файле
+//     print_dialog();
+//   }
 
-  return 0;
+return 0;
 }
 
 int save_token(char *buffer, std::vector<Dialog> &dialogs, Key key) {
@@ -119,8 +123,8 @@ int save_token(char *buffer, std::vector<Dialog> &dialogs, Key key) {
       break;
 
     case FROM:
-      // кладем в последнее сообщение последнего диалога в поле from значение из
-      // буффера
+      // кладем в последнее сообщение последнего диалога в поле from значение
+      // из буффера
       break;
       // ...
   }
